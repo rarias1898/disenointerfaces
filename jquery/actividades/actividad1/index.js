688894867665f8214e231fb5c9551e221682019f
aaridoms <angadominguez96@gmail.com>
2025-02-08 19:44:43 +0100

    Actualiza clases de entrada en el formulario y mejora la lógica de habilitación de botones para Cliente y Proveedor.

--- jquery/actividades/actividad1/index.js
+++ jquery/actividades/actividad1/index.js
@@ -39,31 +39,64 @@ $(function() {
     }
   });
 
-  $(".nombre").on("input", function() {
-    $(".correo").on("input", function() {
-      $(".password").on("input", function() {
-        $(".repitPassword").on("input", function() {
-          $(".confirm").on("input", function() {
-            $(".boton-cliente").removeAttr("disabled")
-          })
-        })
-      })
-    })
-  })
+  // $(".nombre").on("input", function() {
+  //   $(".correo").on("input", function() {
+  //     $(".password").on("input", function() {
+  //       $(".repitPassword").on("input", function() {
+  //         $(".confirm").on("input", function() {
+  //           $(".boton-cliente").removeAttr("disabled")
+  //         })
+  //       })
+  //     })
+  //   })
+  // })
 
-  $(".nombre").on("input", function() {
-    $(".correo").on("input", function() {
-      $(".password").on("input", function() {
-        $(".repitPassword").on("input", function() {
-          $(".fecha").on("input", function() {
-            $(".confirm").on("input", function() {
-              $(".boton-proveedor").removeAttr("disabled")
-            })
-          })
-        })
-      })
-    })
-  })
+  // $(".nombre").on("input", function() {
+  //   $(".correo").on("input", function() {
+  //     $(".password").on("input", function() {
+  //       $(".repitPassword").on("input", function() {
+  //         $(".fecha").on("input", function() {
+  //           $(".confirm").on("input", function() {
+  //             $(".boton-proveedor").removeAttr("disabled")
+  //           })
+  //         })
+  //       })
+  //     })
+  //   })
+  // })
+
+  $(".nombree, .correoo, .passwordd, .repitPassword, .confirm").on("input", function () {
+    console.log("Evento activado para Cliente.");
+
+    let nombre = $(".nombree").val();
+    let correo = $(".correoo").val();
+    let password = $(".passwordd").val().trim();
+    let repitPassword = $(".repitPassword").val().trim();
+    let confirm = $(".confirm").is(":checked");
+
+    if (nombre !== "" && correo !== "" && password !== "" && repitPassword !== "" && confirm) {
+        $(".boton-cliente").prop("disabled", false);
+    } else {
+        $(".boton-cliente").prop("disabled", true);
+    }
+  });
+
+  $(".nombrea, .correoa, .passworda, .repitPassworda, .fechaa, .confirm").on("input", function () {
+    console.log("Evento activado para Proveedor.");
+
+    let nombre = $(".nombrea").val();
+    let correo = $(".correoa").val();
+    let password = $(".passworda").val().trim();
+    let repitPassword = $(".repitPassworda").val().trim();
+    let fecha = $(".fechaa").val();
+    let confirm = $(".confirm").is(":checked");
+
+    if (nombre !== "" && correo !== "" && password !== "" && repitPassword !== "" && fecha !== "" && confirm) {
+        $(".boton-proveedor").prop("disabled", false);
+    } else {
+        $(".boton-proveedor").prop("disabled", true);
+    }
+  });
 
   $(".boton-cliente, .boton-proveedor").on("click", function(event) {
     event.preventDefault()
@@ -72,4 +105,20 @@ $(function() {
       $(".gracias").slideDown("slow")
     })
   })
+
+  $(document).ready(function() {
+    $(".ojo").click(function() {
+        let passwordInput = $(".repitPassword, .password");
+        let type = passwordInput.attr("type") === "password" ? "text" : "password";
+        passwordInput.attr("type", type);
+
+        let eyeIcon = $(this);
+        if (type === "text") {
+            eyeIcon.attr("src", "https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-ios7-eye-512.png");
+        } else {
+            eyeIcon.attr("src", "https://cdn1.iconfinder.com/data/icons/jumpicon-basic-ui-line-1/32/-_Eye-Show-View-Watch-See-Disable-Inactive-Unavailable-Off-256.png");
+        }
+    });
+  });
+
 }); 
