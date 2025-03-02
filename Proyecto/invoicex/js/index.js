c62209404f74393259446f0a9f7e03595b2d765e
aaridoms <angadominguez96@gmail.com>
2025-03-02 03:07:58 +0100

    Mejora la gestión de cookies, añade enlaces en el login y ajusta estilos de interfaz.

--- Proyecto/invoicex/js/index.js
+++ Proyecto/invoicex/js/index.js
@@ -1,10 +1,13 @@
 $(function () {
   if (sessionStorage.getItem("cookie") != 1) {
     $(".privacidad").show();
+
+    $(".toblur").addClass("blur");
   }
 
   $(".btn").on("click", function () {
     $(".privacidad").hide();
+    $(".toblur").removeClass("blur");
 
     sessionStorage.setItem("cookie", 1);
   });
